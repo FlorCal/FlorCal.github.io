85ffabfd406989948b9ae98e4249daa04bd945e0
Flor Calderon <maryf82@yahoo.com>
2018-06-10 22:14:02 -0700

    image loader for Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,14 +25,14 @@ module.exports = {
         ]
       },
       {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [{
-              loader: 'file-loader',
-              options: {
-                  name: '[name].[ext]',
-                  outputPath: 'fonts/'
-              }
-          }]
+        test: /\.(gif|png|jpe?g|woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/'
+            }
+        }]
       }
     ]
   },
