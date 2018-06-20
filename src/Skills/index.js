c2aca1a9c8a850b973325fd63890568159fc6fb2
Flor Calderon <maryf82@yahoo.com>
2018-06-19 21:36:08 -0700

    Moved skills into constants

--- src/Skills/index.js
+++ src/Skills/index.js
@@ -2,44 +2,30 @@ import React from 'react'
 import Ribbon from '../app/Ribbon'
 import Skill from './Skill'
 
+import constants from '../app/constants'
+
 class SkillsComponent extends React.Component {
   render() {
     return (
       <div className='SkillsComponent'>
         <Ribbon title='Skills'/>
         <div className='group1'>
-          <Skill
-            title='JavaScript / ES6'
-            points={7.5} />
-          <Skill
-            title='React'
-            points={7.1} />
-          <Skill
-            title='jQuery'
-            points={6.2} />
-          <Skill
-            title='D3 / SVG'
-            points={4.5} />
-          <Skill
-            title='Node / Express'
-            points={3.9} />
+          {constants.skills.group1.map((skill, i) => (
+            <Skill
+              key={i}
+              title={skill[0]}
+              points={skill[1]} />
+          ))}
+
         </div>
         <div className='group2'>
-          <Skill
-            title='HTML5'
-            points={9.3} />
-          <Skill
-            title='CSS3 / Sass'
-            points={8.1} />
-          <Skill
-            title='Git / GitHub'
-            points={5.2} />
-          <Skill
-            title='Scrum / Trello'
-            points={9.1} />
-          <Skill
-            title='Sketch / Design'
-            points={6.5} />
+          {constants.skills.group2.map((skill, i) => (
+            <Skill
+              key={i}
+              title={skill[0]}
+              points={skill[1]} />
+          ))}
+
         </div>
       </div>
     );
--- src/app/constants.js
+++ src/app/constants.js
@@ -1,10 +1,28 @@
-export default {
-  urls: {
-    myResume: 'https://docs.google.com/document/d/1zMPzSCq54XSOGFKlDuBzbet3U_oo-87ZB6R_MieDJGA/edit?usp=sharing',
-    myGit:
-    'https://github.com/FlorCal',
-    myLinkedin:
-    'https://www.linkedin.com/in/flor-calderon-8280b0114'
-  },
+var constants = {
+    urls: {
+        myResume: 'https://docs.google.com/document/d/1zMPzSCq54XSOGFKlDuBzbet3U_oo-87ZB6R_MieDJGA/edit?usp=sharing',
+        myGit:
+        'https://github.com/FlorCal',
+        myLinkedin:
+        'https://www.linkedin.com/in/flor-calderon-8280b0114'
+    },
+    skills: {
+        group1: [
+          ['JavaScript / ES6', 7.5],
+          ['React', 7.1],
+          ['jQuery', 6.2],
+          ['D3 / SVG', 4.5],
+          ['Node / Express', 3.9]
+        ],
+        group2: [
+          ['HTML5', 9.3],
+          ['CSS3 / Sass', 8.1],
+          ['Git / GitHub', 5.2],
+          ['Scrum / Trello', 9.1],
+          ['Sketch / Design', 6.5]
+        ]
+    }
 
 }
+
+export default constants
