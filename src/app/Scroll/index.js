0e642fb5edca0a30a367081354b271f3db32f2f9
Flor Calderon <maryf82@yahoo.com>
2018-06-22 13:56:31 -0700

    Scroll ribbon working in ScrollComponent.YAY!

--- src/app/Scroll/index.js
+++ src/app/Scroll/index.js
@@ -4,9 +4,15 @@ import React from 'react'
 
 
 class ScrollComponent extends React.Component {
+
+    onScrollClick() {
+        window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    }
+    
     render() {
         return(
-            <div className='ScrollComponent'>
+            <div className='ScrollComponent'
+                onClick={this.onScrollClick.bind(this)}>
             </div>
         );
     }
