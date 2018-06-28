be1958ae0d8befb8ac6298c47e5afd89f6345d53
Flor Calderon <maryf82@yahoo.com>
2018-06-28 11:24:30 -0700

    Grid completed for form in ContactComponent. FormSpree form incorporated and styled in ContactComponent.

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -11,8 +11,29 @@ class ContactComponent extends React.Component {
                 <Ribbon title='Contact'/>
                 <div className='map'>Google Map</div>
 
-                <div className='form'>
-                </div>
+                <form className='form' action="https://formspree.io/calderonflor82@gmail.com"
+                    method="POST">
+                    <span>Write me a note and how to contact you back</span>
+
+                    <div className='label'>
+                        <div className='email'>
+                            <label>Your e-mail:</label>
+                        </div>
+                        <div className='message'>
+                            <label>Message:</label>
+                        </div>
+                    </div>
+
+                    <div>
+                        <input type="email" name="_replyto"/>
+                        <input type="text" name="messagebox"/>
+                        <input type="submit" value="Send Message"/>
+                    </div>
+
+
+
+                </form>
+
             </div>
         )
     }
